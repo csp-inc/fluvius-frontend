c65d303d28dcc79ae97102bc535d8283780613f4
Eric Stofferahn <eric@csp-inc.org>
2022-02-24 00:55:20 +0000

    Edited Layout for About and added Bacon Ipsum for Methodology

--- ms-brazil/src/pages/About.js
+++ ms-brazil/src/pages/About.js
@@ -5,38 +5,27 @@ import {makeStyles} from '@material-ui/styles';
 const useStyles = makeStyles((theme) => ({
     box: {
         minWidth: 900,
-        minHeight: 975,
-        backgroundColor: "black"
-    },
-    stationDetails: {
-        width: "430px",
-        marginLeft: "30px",
         color: "white",
         fontWeight: 800,
-        padding: "25px",
+        fontSize: 48,
+	padding: '25px',
         backgroundColor: "black"
-  }
+    }
   }));
-
 
 const About = () => {
   const classes = useStyles();
   return (
-    <div className="App">
-      <Box display="flex" flexWrap="wrap" flexDirection="row" justifyContent="center" alignItems="center" >
-    <Paper elevation={3} className={classes.box}>
-      <Paper className={classes.stationDetails} elevation={0}>
-        <br></br>
-          <Typography variant="body1">
-            <br></br>
-                <b>Project Overview:</b>
-                <p>Project Fluvius uses satellite images and AI to monitor the health of Amazon and U.S. rivers by enabling near real-time prediction of suspended sediment concentration. </p>
-          </Typography>
+    <Box display="flex" flexWrap="wrap" flexDirection="row" justifyContent="center" alignItems="center" >
+      <Paper elevation={1} className={classes.box}>
+        <Typography align="center" variant="h6">
+	  About the Project
+	</Typography>
+        <Typography variant="body1">
+          <p>The process of soil erosion involves the removal of soil materials from a site by natural and anthropogenic factors. Natural factors include wind and water, but human activity -- deforestation, for instance -- can increase rates of erosion well above natural, background levels. Increased erosion causes environmental damage, including greater sediment loads in the watershed, which in turn negatively impact water quality and aquatic ecosystems, in addition to creating social and economic losses. Traditionally, sediment loads are monitored by assessing the concentration of sediment suspended in water samples. However, ground-based measurements can be slow, hazardous, and costly to obtain, and produce sparse datasets that make monitoring and change detection difficult. Improving our understanding of hydrosedimentological processes will require higher frequency information at much larger spatial extents. The use of satellite imagery to characterize sediment loads is a well-recognized, increasingly effect complement to ground-based sampling. The fluvius project aims to develop tools to monitor hydrosedimenological processes in rivers, including suspended sediment. We improve on ground-based observations by using Artificial Intelligence models to make predictions of suspended sediment at any site and any time for which there is reliable imagery. A pilot study is being carried out in the Itacaiúnas River Basin (BHRI). Preliminary results highlight the potential of the approach, enabling future expansion of monitoring for the entire Amazon basin.</p>
+        </Typography>
       </Paper>
-
-      </Paper>
-      </Box>
-    </div>
+    </Box>
   );
 }
 
--- ms-brazil/src/pages/Methodology.js
+++ ms-brazil/src/pages/Methodology.js
@@ -1,13 +1,31 @@
 import "../App.css";
-import {Box} from "@material-ui/core";
+import {Box, Paper, Typography} from "@material-ui/core";
+import {makeStyles} from '@material-ui/styles';
 
-function Methodology() {
+const useStyles = makeStyles((theme) => ({
+    box: {
+        minWidth: 900,
+        color: "white",
+        fontWeight: 800,
+        fontSize: 48,
+	padding: '25px',
+        backgroundColor: "black"
+    }
+  }));
+
+const Methodology = () => {
+  const classes = useStyles();
   return (
-    <div className="Home">
-      <Box display="flex" flexWrap="wrap" flexDirection="row" justifyContent="center" alignItems="center" >
-	  <div>Methodology Ipsum</div>
-      </Box>
-    </div>
+    <Box display="flex" flexWrap="wrap" flexDirection="row" justifyContent="center" alignItems="center" >
+      <Paper elevation={1} className={classes.box}>
+        <Typography align="center" variant="h6">
+	  Methodology
+	</Typography>
+        <Typography variant="body1">
+          <p>Beef chicken pork bacon chuck shortloin sirloin shank id, ea pig porkloin tail velit excepteur shortribs, brisket esse adipisicing labore consectetur flank tongue. Quis porkchop adipisicing proident tongue biltong porkbelly in prosciutto culpa, shank chicken ut hamhock excepteur turducken esse sausage. Excepteur sirloin cornedbeef aliqua flank in in porkchop leberkas tail, tempor nulla filetmignon veniam reprehenderit porchetta beef ea deserunt, duis shoulder in laborum est ham aute et. Consequat veniam in do capicola sirloin cow cupidatat adipisicing, pork velit cillum venison pariatur mollit jerky exercitation, sint tenderloin sunt labore jowl in aliqua. Excepteur kielbasa jowl ut fatback exercitation bresaola cow, boudin voluptate turducken est proident nisi filetmignon dolore, leberkas prosciutto porkbelly culpa tri-tip occaecat. Biltong beefribs dolore filetmignon turkey ut beef prosciutto, esse shankle andouille adipisicing shortribs nostrud excepteur, leberkas landjaeger tail hamburger eiusmod officia. Eiusmod spareribs swine beefribs eu dolore elit veniam shoulder flank incididunt, cow non andouille tongue consectetur laboris nostrud pastrami proident sirloin aute, ut meatball sed dolor officia voluptate consequat shortloin sunt.</p>
+        </Typography>
+      </Paper>
+    </Box>
   );
 }
 
