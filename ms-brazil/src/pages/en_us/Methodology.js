1f15e654568a93edf87882bc0bfc76aa38d9826f
Eric Stofferahn <eric@csp-inc.org>
2022-03-16 19:41:59 +0000

    Updated methodology baseed on document.

--- ms-brazil/src/pages/en_us/Methodology.js
+++ ms-brazil/src/pages/en_us/Methodology.js
@@ -26,11 +26,26 @@ const Methodology = () => {
 	  Methodology
 	</Typography>
         <Typography className={classes.text}>
-          <p>The goal of the Fluvius model was to enable predictions of suspended sediment concentration (SSC) from Sentinel 2 satellite imagery. We extracted image "chips" for all dates and locations for which we had SSC observations from USGS, USGSI, ITV, and ANA gauging stations, and used the Sentinel 2 bands of that image as input to a multi-layer perceptron model (MLP), which is a type of neural network, to predict SSC. After training and validating the model, we were able to make predictions of SSC not only for sites and times at which we have ground-based measures, but at sites and times for which such measures are lacking entirely.</p>
+          <p>The goal of the Fluvius project is to enable estimation of water quality given satellite imagery of rivers. One major metric of water quality monitoring is the measure of suspended sediment concentration (SSC)[https://www.usgs.gov/special-topics/water-science-school/science/sediment-and-suspended-sediment#overview], which is the measure of solid-phase material within a fixed water column. SSC analysis is a common indicator of water quality for aquatic and riparian systems because sediment is the most widespread cause of impairment of rivers and streams, lakes, reservoirs, ponds, and estuaries. As SSC in water increases or decreases due to landscape level modification such as deforestation or presence of agriculture, the visual quality of the water changes as well.</p>
+	  <p>In this analysis ITV, in collaboration with CSP and Microsoft, used satellite based images called image “chips” that were taken at the same location and time of SSC sampling on the ground from gaging stations. We used the light characteristics of these images chips and field measurements to train and validate a deep learning model. With this properly trained model, we can make predictions of SSC not only for sites and times at which we have ground-based measurements, but also to sites and times at which such measurements are lacking entirely.</p>
         </Typography>
-      <Typography align="center">
-        <img class="big-img" alt="" src={amazon_methods}/>
-      </Typography>
+        <Typography align="center" variant="h6">
+	  The Data
+	</Typography>
+        <Typography className={classes.text}>
+	  <p>The source data for this modeling effort came from in-situ (ground based) observations of SSC from rivers and streams in both Brazil and the United States. We compiled data from ITV, ANA[https://www.gov.br/ana/en], and the USGS[https://waterdata.usgs.gov/nwis/rt] and converted SSC measurements for each to units of milligrams per liter (mg/L). Each SSC observation has associated metadata and coordinates describing the time and site at which the measurement was taken. We used geolocation and timestamp information to associate each observation with a Sentinel 2 [https://www.esa.int/Applications/Observing_the_Earth/Copernicus/Sentinel-2] satellite image to use for model training, wherein the spectral characteristics within water pixels in the image chip were features in the model, and the associated SSC measurement (log-transformed) is the response. We used data from the Sentinel-2 L2A image archived in the Microsoft Planetary Computer[https://planetarycomputer.microsoft.com/dataset/sentinel-2-l2a#overview].</p>
+        </Typography>
+        <Typography align="center" variant="h6">
+	  The Model
+	</Typography>
+        <Typography className={classes.text}>
+	  <p>We trained a deep learning model, specifically a Deep Neural Network [https://docs.microsoft.com/en-us/azure/machine-learning/concept-deep-learning-vs-machine-learning], using the feature data we extracted for the SSC observations using the PyTorch library in Python. 
+Following the identification of the top model, we extracted image chips for the entire Sentinel-2 L2A archive at each Brazilian water monitoring station to deploy our model and generate time series predictions of SSC.</p>
+	  <p>More information on methods and results can be found in [LINK TO REPORT].</p>
+        </Typography>
+        <Typography align="center">
+          <img class="big-img" alt="" src={amazon_methods}/>
+        </Typography>
       </Paper>
     </Box>
   );
